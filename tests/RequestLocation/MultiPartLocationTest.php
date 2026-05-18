0e50040ed963624c5537607c12db2236fbe92e65
Graham Campbell <hello@gjcampbell.co.uk>
2026-05-18 22:00:08 +0100

    Fix multipart Content-Type header
    Co-authored-by: uikolas <6900701+uikolas@users.noreply.github.com>

--- tests/RequestLocation/MultiPartLocationTest.php
+++ tests/RequestLocation/MultiPartLocationTest.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Command\Command;
 use GuzzleHttp\Command\Guzzle\Operation;
 use GuzzleHttp\Command\Guzzle\Parameter;
 use GuzzleHttp\Command\Guzzle\RequestLocation\MultiPartLocation;
+use GuzzleHttp\Psr7\MultipartStream;
 use GuzzleHttp\Psr7\Request;
 use PHPUnit\Framework\TestCase;
 
@@ -26,9 +27,31 @@ class MultiPartLocationTest extends TestCase
         $request = $location->visit($command, $request, $param);
         $operation = new Operation();
         $request = $location->after($command, $request, $operation);
+        $body = $request->getBody();
+        $actual = $body->getContents();
+
+        $this->assertNotFalse(strpos($actual, 'name="foo"'));
+        $this->assertNotFalse(strpos($actual, 'bar'));
+        $this->assertInstanceOf(MultipartStream::class, $body);
+        $this->assertSame('multipart/form-data; boundary='.$body->getBoundary(), $request->getHeaderLine('Content-Type'));
+    }
+
+    /**
+     * @group RequestLocation
+     */
+    public function testVisitsLocationDoesNotOverwriteContentTypeHeader()
+    {
+        $location = new MultiPartLocation();
+        $command = new Command('foo', ['foo' => 'bar']);
+        $request = new Request('POST', 'http://httbin.org', ['Content-Type' => 'application/vnd.example']);
+        $param = new Parameter(['name' => 'foo']);
+        $request = $location->visit($command, $request, $param);
+        $operation = new Operation();
+        $request = $location->after($command, $request, $operation);
         $actual = $request->getBody()->getContents();
 
         $this->assertNotFalse(strpos($actual, 'name="foo"'));
         $this->assertNotFalse(strpos($actual, 'bar'));
+        $this->assertSame('application/vnd.example', $request->getHeaderLine('Content-Type'));
     }
 }
