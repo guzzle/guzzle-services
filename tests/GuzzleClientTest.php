b544ffd20cc34b9b67b260c83b5da0d0252712f0
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-09 12:11:27 +0100

    Avoid HandlerStack string assertions

--- tests/GuzzleClientTest.php
+++ tests/GuzzleClientTest.php
@@ -6,6 +6,7 @@ namespace GuzzleHttp\Tests\Command\Guzzle;
 
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Command\CommandInterface;
+use GuzzleHttp\Command\Exception\CommandException;
 use GuzzleHttp\Command\Guzzle\Description;
 use GuzzleHttp\Command\Guzzle\GuzzleClient;
 use GuzzleHttp\Command\Guzzle\ResponseLocation\XmlLocation;
@@ -373,8 +374,21 @@ class GuzzleClientTest extends TestCase
 
     public function testAddsValidateHandlerWhenTrue(): void
     {
-        $client = new HttpClient();
-        $description = new Description([]);
+        $client = new HttpClient([
+            'handler' => new MockHandler([new Response(200, [], '{"ok":true}')]),
+        ]);
+        $description = new Description([
+            'operations' => [
+                'Foo' => [
+                    'parameters' => [
+                        'bar' => [
+                            'type' => 'string',
+                            'required' => true,
+                        ],
+                    ],
+                ],
+            ],
+        ]);
         $guzzle = new GuzzleClient(
             $client,
             $description,
@@ -387,15 +401,29 @@ class GuzzleClientTest extends TestCase
             ]
         );
 
-        $handlers = explode("\n", $guzzle->getHandlerStack()->__toString());
-        $handlers = array_filter($handlers);
-        $this->assertCount(3, $handlers);
+        $this->expectException(CommandException::class);
+        $this->expectExceptionMessage('Validation errors: [bar] is a required string');
+
+        $guzzle->execute($guzzle->getCommand('Foo'));
     }
 
-    public function testDisablesHandlersWhenFalse(): void
+    public function testSkipsValidationWhenFalse(): void
     {
-        $client = new HttpClient();
-        $description = new Description([]);
+        $client = new HttpClient([
+            'handler' => new MockHandler([new Response(200, [], '{"ok":true}')]),
+        ]);
+        $description = new Description([
+            'operations' => [
+                'Foo' => [
+                    'parameters' => [
+                        'bar' => [
+                            'type' => 'string',
+                            'required' => true,
+                        ],
+                    ],
+                ],
+            ],
+        ]);
         $guzzle = new GuzzleClient(
             $client,
             $description,
@@ -408,9 +436,10 @@ class GuzzleClientTest extends TestCase
             ]
         );
 
-        $handlers = explode("\n", $guzzle->getHandlerStack()->__toString());
-        $handlers = array_filter($handlers);
-        $this->assertCount(1, $handlers);
+        $result = $guzzle->execute($guzzle->getCommand('Foo'));
+
+        $this->assertSame(true, $result['ok']);
+        $this->assertSame('Foo', $result['_request']['action']);
     }
 
     public function testValidateDescription(): void
