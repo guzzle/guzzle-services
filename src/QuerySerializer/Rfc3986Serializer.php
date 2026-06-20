9c79b80df690ad9f04ac5d705a26f7dc5c28aade
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-20 21:48:23 +0100

    Align query serializer PCRE guard style

--- src/QuerySerializer/Rfc3986Serializer.php
+++ src/QuerySerializer/Rfc3986Serializer.php
@@ -27,13 +27,11 @@ class Rfc3986Serializer implements QuerySerializerInterface
         $queryString = http_build_query($queryParams, '', '&', PHP_QUERY_RFC3986);
 
         if ($this->removeNumericIndices) {
-            $normalized = preg_replace('/%5B[0-9]+%5D/simU', '%5B%5D', $queryString);
+            $queryString = preg_replace('/%5B[0-9]+%5D/simU', '%5B%5D', $queryString);
 
-            if ($normalized === null) {
+            if ($queryString === null) {
                 throw new \RuntimeException('Unable to normalize query string: '.preg_last_error_msg());
             }
-
-            $queryString = $normalized;
         }
 
         return $queryString;
